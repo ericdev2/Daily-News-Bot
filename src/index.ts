73882c56d74bf7eb70073b86472f701a9b0c438b
Eric Arakelian <ericarakelian@Mac.attlocal.net>
2025-04-29 22:18:13 -0700

    Fix: run bot only on schedule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { scheduleDailyReport } from './scheduler';
-import { sendDiscordMessage } from './discord';
-import { generateMarketReport } from './scraper/news';
+import { runBot } from './scraper/news';
+import cron from 'node-cron';
 
-(async () => {
-  const message = await generateMarketReport();
-  await sendDiscordMessage(message);
-  scheduleDailyReport();
-})();
+console.log('Bot started and waiting for next scheduled run...');
+
+cron.schedule('0 8 * * *', () => {
+  console.log('Running bot at scheduled time...');
+  runBot();
+}, {
+  timezone: 'America/Los_Angeles',
+});
